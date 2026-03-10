70627ba6f56eaaad889ebfd1bcb4d9e2f14cb44e
infofvalentim <info.fvalentim@gmail.com>
2026-03-10 14:39:42 -0300

    Implement reveal function for scroll animation
    Add scroll animation to reveal elements on scroll

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const nav = document.getElementById("nav");
 nav.classList.toggle("active");
 
 }
+
+/* scroll animation */
+
+function reveal(){
+
+let reveals = document.querySelectorAll(".reveal");
+
+for(let i=0;i<reveals.length;i++){
+
+let windowHeight = window.innerHeight;
+
+let elementTop = reveals[i].getBoundingClientRect().top;
+
+let elementVisible = 100;
+
+if(elementTop < windowHeight - elementVisible){
+
+reveals[i].classList.add("active");
+
+}
+
+}
+
+}
+
+window.addEventListener("scroll", reveal);
